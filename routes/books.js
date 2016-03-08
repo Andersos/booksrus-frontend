e5076133258462516084ba462f9f3e4e9763a596
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2016-03-08 15:41:40 +0100

    book-bff

---
+++ routes/books.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var jp = require('jsonpath');
+var router = express.Router();
+var goodGuy = require('good-guy-http')({maxRetries: 3});
+
+var BOOK_SERVICE_URL = 'https://book-catalog-proxy-4.herokuapp.com/book?isbn=';
+
+router.get('/:isbn', function(req, res, next) {
+  goodGuy({url: `${BOOK_SERVICE_URL}${req.params.isbn}`, json: true}).then((data) => {
+    var pageData = {
+      title: jp.value(data.body, '$..title'),
+      subtitle: jp.value(data.body, '$..subtitle'),
+      cover: jp.value(data.body, '$..thumbnail')
+    };
+    res.render('book', pageData);
+  })
+});
+
+module.exports = router;
