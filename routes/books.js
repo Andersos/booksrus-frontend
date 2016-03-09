7ff3bf861200bfc65caf5c18516573e59b563a37
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2016-03-09 10:20:37 +0100

    Add headers and client side rendering

--- routes/books.js
+++ routes/books.js
@@ -6,11 +6,17 @@ var goodGuy = require('good-guy-http')({maxRetries: 3});
 var BOOK_SERVICE_URL = 'https://book-catalog-proxy-4.herokuapp.com/book?isbn=';
 
 router.get('/:isbn', function(req, res, next) {
+  req.esiOptions = {
+    headers: {
+      'Accept': 'text/html'
+    }
+  };
   goodGuy({url: `${BOOK_SERVICE_URL}${req.params.isbn}`, json: true}).then((data) => {
     var pageData = {
       title: jp.value(data.body, '$..title'),
       subtitle: jp.value(data.body, '$..subtitle'),
-      cover: jp.value(data.body, '$..thumbnail')
+      cover: jp.value(data.body, '$..thumbnail'),
+      isbn: req.params.isbn
     };
     res.render('book', pageData);
   })
